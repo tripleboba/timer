2c000fd7cbd89676ac4de4df65e5fab4657a8757
Triple the Boba ! <coconuttt1010@gmail.com>
2021-12-02 03:07:47 -0800

    need to make the terminal countdown before beep later

---
+++ timer.js
@@ -0,0 +1,18 @@
+/**
+ * Implement an alarm clock / timer 
+ * which will beep after a specified amount of time has passed.
+ * The user can specify an unlimited number of alarms using command line arguments
+ * 
+ *  NEED TO RUN ON COMPUTER'S TERMINAL TO WORK! not through visual code's terminal
+ */
+
+const timeSet = process.argv.splice(2);
+for (let time of timeSet) {
+  if (!isNaN(time) && time > 0) {
+  // console.log(`Time set @: ${time} secs`);
+    setTimeout(() => { 
+      process.stdout.write('\x07'); 
+    }, time * 1000,
+    console.log(`Time set @: ${time} secs to beep`));
+  }
+}
